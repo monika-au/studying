3d13354d861e3d35723353ab83035d69e06ff91f
Monika A <147951366+monika-au@users.noreply.github.com>
2024-02-24 08:48:37 +0100

    Delete lesson directory
    deleting unused files

--- lesson/mod02/cw2.6.py
+++
@@ -1,8 +0,0 @@
-name = "United Nations Educational, Scientific and Cultural Organization"
-shortcut = "" #pusty tekst
-splitted = name.split(sep=" ")
-
-print(splitted)
-
-list = "".join(['{:.1}'.format(word) for word in splitted if word[0].isupper()])
-print(list)
